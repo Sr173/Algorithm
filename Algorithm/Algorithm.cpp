0fed45e1c6d6df675dc5be9db90490a4ae2f0e7a
sr173 <66456804@qq.com>
2019-03-13 19:34:56 +0800

    提交

---
+++ Algorithm/Algorithm.cpp
@@ -0,0 +1,121 @@
+﻿#include<iostream>
+
+#include<stdlib.h>
+#include "algorithm"
+#include<time.h>
+
+#define MAX_VALUE 10000
+#define random() rand()%MAX_VALUE
+#define N 100
+
+int a[N];
+
+using namespace std;
+
+class RBTreeNode {
+	enum Color {
+		red,
+		black
+	};
+public:
+	RBTreeNode(int val) : val(val) {
+
+	}
+	RBTreeNode* right;
+	RBTreeNode* left;
+	RBTreeNode* parent;
+	Color color;
+	int val;
+};
+
+class RBTree {
+public:
+	RBTree() {
+
+	}
+	auto insert(RBTreeNode* node,bool isLeft,int val) {
+		auto newNode =new RBTreeNode(val);
+		if (root == nullptr) {
+			root = newNode;
+			return newNode;
+		}
+		if (isLeft) {
+			node->left = newNode;
+		}
+		else {
+			node->right = newNode;
+		}
+		return newNode;
+	}
+	void lRotate(RBTreeNode* node) {
+		auto target = node->right;
+		node->right = target->left;
+		if (target->left != nullptr) {
+			target->left->parent = node;
+		}
+		target->parent = node->parent;
+		if (node->parent == nullptr) {
+			root = target;
+		}
+		else if (node == node->parent->left) {
+			node->parent->left = target;
+		}
+		else {
+			node->parent->right = target;
+		}
+		target->left = node;
+		node->parent = target;
+	}
+	void rRotate(RBTreeNode* node) {
+		auto target = node->left;
+		node->left = target->right;
+		if (target->right != nullptr) {
+			target->right->parent = node;
+		}
+		target->parent = node->parent;
+		if (node->parent == nullptr) {
+			root = target;
+		}
+		else if (node == node->parent->left) {
+			node->parent->left = target;
+		}
+		else {
+			node->parent->right = target;
+		}
+		target->right = node;
+		node->parent = target;
+	}
+
+	void print() {
+		printTree(root);
+		cout << endl;
+	}
+
+private:
+	void printTree(RBTreeNode* node) {
+		if (node == nullptr)
+			return;
+		cout << node->val << " " ;
+		printTree(node->left);
+		printTree(node->right);
+	}
+	RBTreeNode* root = nullptr;
+};
+
+void main()
+{
+	RBTree* rbTree = new RBTree();
+	auto node = rbTree->insert(0, true, 1);
+	auto node_2 = rbTree->insert(node, true, 2);
+	auto node_3 = rbTree->insert(node, false, 3);
+	rbTree->insert(node_2, true, 4);
+	rbTree->insert(node_2, false, 5);
+	rbTree->insert(node_3, true, 6);
+	rbTree->insert(node_3, false, 7);
+	rbTree->print();
+	rbTree->rRotate(node);
+	//rbTree->lRotate(node);
+
+	rbTree->print();
+
+}
---
+++ dynamic-programming/dynamic-programming.cpp
@@ -0,0 +1,31 @@
+﻿// dynamic-programming.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "vector"
+
+int price[] = { 1,5,8,9,10,17,17,20,24,30 };
+
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
+int getMax(int n) {
+	if (n <= 0)
+		return 0;
+	int q = -0x37373737;
+	for (int i = 0; i < sizeof(price) / sizeof(price[0]);i++) {
+		q = max(q,price[i] + (getMax(n - i - 1)) - 3);
+	}
+	return q;
+}
+
+int getMax1(int n) {
+
+}
+
+int getMax2(int n) {
+	std::vec
+}
+
+int main() {
+	std::cout << getMax(10) << std::endl; 
+}
